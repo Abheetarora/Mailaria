d6049faa75e0cf0f808d470d3c28c565e0a0c665
Abheet Arora <62665287+Abheetarora@users.noreply.github.com>
2021-06-27 22:32:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,26 +10,27 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
+const MongoStore = require('connect-mongo');
+const sassMiddleware=require('node-sass-middleware');
+const flash=require('connect-flash');
+const customMware=require('./config/middleware')
+// setup the chat server to be used with socket.io
 
-const MongoStore = require('connect-mongo')(session);
-const sassMiddleware = require('node-sass-middleware');
-const flash = require('connect-flash');
-const customMware = require('./config/middleware');
 
 
 app.use(sassMiddleware({
-    src: './assets/scss',
-    dest: './assets/css',
+    src:'./assets/scss',
+    dest:'./assets/css',
     debug: true,
-    outputStyle: 'extended',
-    prefix: '/css'
-}));
+    outputStyle:'extended',
+    prefix:'/css'
+}))
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
-// make the uploads path available to the browser
+//make the upload path available to the browser
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(expressLayouts);
@@ -54,15 +55,16 @@ app.use(session({
     cookie: {
         maxAge: (1000 * 60 * 100)
     },
-    store: new MongoStore(
-        {
-            mongooseConnection: db,
+    store:  MongoStore.create({
+	mongoUrl: 'mongodb://localhost/mailaria',
+    mongooseConnection: db,
             autoRemove: 'disabled'
-        
-        },
-        function(err){
-            console.log(err ||  'connect-mongodb setup ok');
-        }
+
+    },
+    function(err){
+        console.log(err || 'connect mongo db setup');
+    }
+    
     )
 }));
 
@@ -70,9 +72,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
-
 app.use(flash());
 app.use(customMware.setFlash);
+
 
 // use express router
 app.use('/', require('./routes'));
